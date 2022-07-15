920ba7a76b181cafe84c37a2991a8063597c2f3c
KateFesik <109290179+KateFesik@users.noreply.github.com>
2022-07-15 14:03:45 +0200

    querySelector
    in index.html

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 let apiKey = "6044b52d072e537df7be674146654ba7";
+
+let newCity = document.querySelector("#city_name");
+let tempValue = document.querySelector("#temp_value");
+let mainInfo = document.querySelector(".main_info");
+let maxTemp = document.querySelector("#max_temp");
+let minTemp = document.querySelector("#min_temp");
+let humidity = document.querySelector("#value_humidity");
+let wind = document.querySelector("#value_wind");
+let iconWeather = document.querySelector(".icon_weather");
+let degreesMax = document.querySelector("#degrees_max");
+let degreesMin = document.querySelector("#degrees_min");
